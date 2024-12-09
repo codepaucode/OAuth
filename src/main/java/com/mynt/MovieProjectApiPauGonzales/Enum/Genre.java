8c8cc8ebfe9b219f0b91cc951d79bc1abd5a83d8
codepaucode <paulajane.gonzales@gcash.com>
2024-12-09 16:53:02 +0800

    Removed duplicated src folder

--- src/main/java/com/mynt/MovieProjectApiPauGonzales/Enum/Genre.java
+++
@@ -1,9 +0,0 @@
-package com.mynt.MovieProjectApiPauGonzales.Enum;
-
-public enum Genre {
-    ACTION,
-    COMEDY,
-    ROMANCE,
-    HORROR,
-    FANTASY;
-}
--- src/main/java/com/mynt/MovieProjectApiPauGonzales/Repository/GenreRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mynt.MovieProjectApiPauGonzales.Repository;
-
-import com.mynt.MovieProjectApiPauGonzales.Model.Genre;
-import org.springframework.data.repository.CrudRepository;
-
-public interface GenreRepository extends CrudRepository<Genre, Long> {
-    Genre getGenreById(Long genreId);
-}
